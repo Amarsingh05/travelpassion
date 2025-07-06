57679718ebe3e303e6a8aa2f53256b3d4734c286
Amarsingh05 <pamar0522@gmail.com>
2025-07-06 20:45:44 +0530

    initial commit -setup tourism

---
+++ src/main/java/com/travelpassion/TravelpassionApplication.java
@@ -0,0 +1,13 @@
+package com.travelpassion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelpassionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelpassionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travelpassion/TravelpassionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelpassion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelpassionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
